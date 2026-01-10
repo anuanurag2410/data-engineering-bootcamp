692922372a363c6b2b888ad4f43c5e694726445f
anuanurag2410 <anuanurag2410@gmail.com>
2026-01-10 07:10:24 +0000

    test

---
+++ Untitled 1.sql
@@ -0,0 +1,5 @@
+ALTER GIT REPOSITORY datax_bootcamp_repo
+    SET AUTHENTICATION = (
+        TYPE = 'GITHUB'
+        TOKEN = 'ZK3duDYTJvQK91mRE9iRuk+WCtd4Y0DrnAl+cRy13ik'
+    );
---
+++ test.sql
@@ -0,0 +1,3 @@
+CREATE OR REPLACE GIT REPOSITORY datax_bootcamp_repo
+    API_INTEGRATION = dataxbootcampcode
+    ORIGIN = 'https://github.com/anuanurag2410/data-engineering-bootcamp.git';
