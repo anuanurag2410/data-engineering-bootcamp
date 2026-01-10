d5c4f1e60f3259b4f7ccb0d592269642441efe4f
anuanurag2410 <anuanurag2410@gmail.com>
2026-01-10 07:28:07 +0000

    Created the Steps to set GIT on Snowflake

--- Untitled 1.sql
+++
@@ -1,5 +0,0 @@
-ALTER GIT REPOSITORY datax_bootcamp_repo
-    SET AUTHENTICATION = (
-        TYPE = 'GITHUB'
-        TOKEN = 'ZK3duDYTJvQK91mRE9iRuk+WCtd4Y0DrnAl+cRy13ik'
-    );
--- test.sql
+++
@@ -1,3 +0,0 @@
-CREATE OR REPLACE GIT REPOSITORY datax_bootcamp_repo
-    API_INTEGRATION = dataxbootcampcode
-    ORIGIN = 'https://github.com/anuanurag2410/data-engineering-bootcamp.git';
