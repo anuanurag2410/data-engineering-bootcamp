3b8aaa79d84f829dbb2f1779178394e961c33fa9
anuanurag2410 <anuanurag24101997@gmail.com>
2026-07-30 17:01:55 +0000

    Push notes

--- Module 3 - Python/Extension Python Classes/01-Where Python Is Used in Data Engineering/Where Python Is Used in Data Engineering.py
+++ Module 3 - Python/Extension Python Classes/01-Where Python Is Used in Data Engineering/Where Python Is Used in Data Engineering.py
@@ -1,2 +1,20 @@
 # Databricks notebook source
+# MAGIC %md
+# MAGIC ### The Python we are going to learn is specifically for Data Engineering and not for Programming
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC - Read Data
+# MAGIC - Validate the data
+# MAGIC - Clean Data
+# MAGIC - Automate repetetive Task or work 
+# MAGIC - Call APIs 
+# MAGIC - Process Files 
+# MAGIC - Connect Databases 
+# MAGIC - Build Data Pipelines
+# MAGIC - Handle pipeline faliures
+
+# COMMAND ----------
+
 
